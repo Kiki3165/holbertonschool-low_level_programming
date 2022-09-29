e363e62252f1d1c9441805b345a4ddee001af6e0
kyllian <kikikou82@gmail.com>
2022-09-29 06:04:46 -0700

    progC4-2

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/c
 #include <stdio.h>
 
 int main( void )
 
 {
   puts ("\"Programming is like building a multilingual puzzle, followed by a new line")
-   return (0)
+   return(0)
  }
   
