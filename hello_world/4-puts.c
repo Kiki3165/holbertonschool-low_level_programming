4f58f1994b8e07d3d959ed7909fa884751db0833
kyllian <kikikou82@gmail.com>
2022-09-29 06:59:56 -0700

    progC4-11

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
-int main (void)
-  
+/**
+ * main - jsp
+ * Return: 0 works
+ */
+int main(void)
 {
-  puts ("\"Programming is like building a multilingual puzzle");
-
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
- 
 }
