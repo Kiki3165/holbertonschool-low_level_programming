24408539709c23c705d5b781e41c723f27fd281d
kyllian <kikikou82@gmail.com>
2022-10-03 07:12:33 -0700

    task3-1

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print lowercase alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+putchar('\n');
+for (i = 'A'; i <= 'Z'; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
