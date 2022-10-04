a8d0a4f2860c755123a1fd07d5916c90d7ab7c8e
kyllian <kikikou82@gmail.com>
2022-10-04 10:13:04 -0700

    task9-10

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 /**
  * main - print lowercase alphabet
@@ -9,12 +8,11 @@ int main(void)
 {
 int num;
 for (num = '0'; num <= '9'; num++)
-putchar(num);
-{
+
 if (num != '9')
+putchar(num);
 putchar(',');
 putchar(' ');
-}
 putchar('\n');
 return (0);
 }
