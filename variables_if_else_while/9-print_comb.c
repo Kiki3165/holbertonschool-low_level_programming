625f6a8cf3a053bb3416f284169133e107dfe131
kyllian <kikikou82@gmail.com>
2022-10-04 06:00:28 -0700

    task9-2

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,11 +7,9 @@
 int main(void)
 {
 int num;
-for (num = 0; num % 10; num++)
+for (num = '0'; num <= '9'; num++)
 {
-putchar((num % 10) + '0');
-if (num == 9)
-continue;
+if (num == '9')
 putchar(',');
 putchar(' ');
 }
