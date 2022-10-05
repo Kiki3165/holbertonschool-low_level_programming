0dc2ab4193d768884bb3425e889d57b8c229e575
kyllian <kikikou82@gmail.com>
2022-10-05 04:58:20 -0700

    2-main.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * void - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_10(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+for (i = 0; i < 10; i++)
+_putchar(i);
+_putchar('\n');
+}
