db503f381210c95499bc7e5227c6d9f47349a798
kyllian <kikikou82@gmail.com>
2022-10-05 13:11:51 -0700

    task6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -4,10 +4,17 @@
 /**
  * _abs - check the code
  *
+ * @c: The number to be computed.
+ *
  * Return: Always 0.
  */
-int _abs(int)
+int _abs(int c)
 {
-int abs (n);
-return (0);
+if (c < 0)
+{
+int abs_val;
+abs_val = c * -1;
+return (abs_val);
+}
+return (c);
 }
