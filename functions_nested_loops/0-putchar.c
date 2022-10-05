63d3686c26df19477c13066a0c51b6624870ab0b
kyllian <kikikou82@gmail.com>
2022-10-05 02:29:28 -0700

    task0-1

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
+
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - writes the character c to stdout
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: On success 0.
  */
+
 int main(void)
 {
 _putchar('_');
