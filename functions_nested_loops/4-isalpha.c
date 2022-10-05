4678c6cfaaa57a418a33cda7e0737f812fcd211a
kyllian <kikikou82@gmail.com>
2022-10-05 11:10:22 -0700

    task4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -5,9 +5,9 @@
  */
 int _isalpha(void)
 {
-for ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
-    {
-      return (1);
-    }
-  return (0);
+if ((c >= 'a' && c <= 'z') ||
+(c >= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
 }
