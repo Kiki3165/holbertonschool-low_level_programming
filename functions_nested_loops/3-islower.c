8ba73c39a25f189ce4e9d1aa5a56250e8c51b861
Kiki3165 <113670617+Kiki3165@users.noreply.github.com>
2022-10-05 18:11:58 +0200

    Task3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,1 +1,18 @@
+#include "main.h"
+/**
+ * _islower - check the code
+ *
+ * Return: Always 0.
+ */
+int _islower(int c);
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+if (c == i)
+return (1);
+else
+return (0);
+}
+}
 
