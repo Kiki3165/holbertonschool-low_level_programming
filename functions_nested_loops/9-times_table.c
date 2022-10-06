d9b74d824c9e5fd77e0cce8f0f89d2ad10cf4452
kyllian <kikikou82@gmail.com>
2022-10-06 06:11:47 -0700

    task9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -4,14 +4,14 @@
  *
  * Return: Always 0.
  */
-void times_table(void);
+void times_table(void)
 {
 int i, x, result;
 for (i = 0; i <= 9; i++)
 {
 for (x = 0; <= 9; x++)
 {
-  result = i * x;
+result = i * x;
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
