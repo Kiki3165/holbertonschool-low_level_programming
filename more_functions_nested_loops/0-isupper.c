143107c3f155f0134862f3f7995db0dfb5ac25cb
kyllian <kikikou82@gmail.com>
2022-10-07 05:11:31 -0700

    task0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+/**
+ * main - nested loops
+ *
+ * Return: 0
+ */
+int _isupper(int c)
+{
+if ( ((c >= 'A' && c <= 'Z') ||
+(c >= 'a' && c <= 'z'))
+return (1);
+else
+return (0);
+}
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+int _isalpha(int c);
+
+int _isupper(int c);
+
+void times_table(void);
+
+void jack_bauer(void);
+
+int print_last_digit(int);
+
+int _abs(int);
+
+int print_sign(int n);
+
+int _putchar(char c);
+
+void print_alphabet(void);
+
+int _islower(int c);
+
+void print_alphabet_x10(void);
+
+#endif
