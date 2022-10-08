e4c0dbd1754e4c62acfb0492b48aa65556e640c5
kyllian <kikikou82@gmail.com>
2022-10-08 12:16:45 -0700

    task2

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -3,8 +3,8 @@
 /**
  * mul - check the code
  *
- *@a number multiplicate
- *@b number multiplicate
+ *@a: number multiplicate
+ *@b: number multiplicate to the first
  * Return: Always 0.
  */
 int mul(int a, int b)
