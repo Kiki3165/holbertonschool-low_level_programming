cfc279849339210e0ce1abae8bf51c6bb6a04b99
kyllian <kikikou82@gmail.com>
2022-10-08 12:40:24 -0700

    task5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,11 @@ void more_numbers(void)
 int i , c;
 for (i = 0; i < 10; i++)
 {
-for (c = NUL; c <= SO; c++)
+for (c = 0; c <= 14; c++)
 {
-_putchar(c);
+if (num > 9)
+_putchar((num / 10) + '0');
+_putchar((num % 10) + '0');
 }
 _putchar('\n');
 }
