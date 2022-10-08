15ab535422fb1e1ada8500a2afc3e30049db95f6
kyllian <kikikou82@gmail.com>
2022-10-08 12:22:29 -0700

    task5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * more_numbers - check the code
@@ -7,12 +8,12 @@
  * Return: Always 0.
  */
 void more_numbers(void)
+{
 int i,c;
 for (i = 0; i < 10; i++)
 {
 for (c = 0; c <= 14; c++)
 {
-_putchar(i);
 _putchar(c);
 }
 _putchar('\n');
