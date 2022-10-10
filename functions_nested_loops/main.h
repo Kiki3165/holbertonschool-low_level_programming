410c5ba0272aa04e157b8fda9ce0b2543d91815a
kyllian <kikikou82@gmail.com>
2022-10-10 03:57:29 -0700

    10-add.c

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,6 +5,26 @@
 
 int _isalpha(int c);
 
+int _strlen(char *s);
+
+void swap_int(int *a, int *b);
+
+void reset_to_98(int *n);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+int add(int, int);
+
+int mul(int a, int b);
+
+void print_numbers(void);
+
+int _isdigit(int c);
+
+int _isupper(int c);
+
 void times_table(void);
 
 void jack_bauer(void);
