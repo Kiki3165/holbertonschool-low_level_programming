41e1d676f0a5b86dca0be8a82ead5b3a235819fa
kyllian <kikikou82@gmail.com>
2022-10-10 00:51:59 -0700

    task0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - nested loops
+ *
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+int *n;
+n = 98
+return (0);
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,40 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+int _isalpha(int c);
+
+void reset_to_98(int *n);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+int mul(int a, int b);
+
+void print_numbers(void);
+
+int _isdigit(int c);
+
+int _isupper(int c);
+
+void times_table(void);
+
+void jack_bauer(void);
+
+int print_last_digit(int);
+
+int _abs(int);
+
+int print_sign(int n);
+
+int _putchar(char c);
+
+void print_alphabet(void);
+
+int _islower(int c);
+
+void print_alphabet_x10(void);
+
+#endif
