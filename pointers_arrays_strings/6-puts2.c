a6c0008153e2e60ac0d3f0f40bcab18afd028b17
kyllian <kikikou82@gmail.com>
2022-10-11 11:14:43 -0700

    task6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,13 @@
  */
 void puts2(char *str)
 {
-int i = 0;
-while (*(str + i))
+int i;
+for (i = 0; str[i] != '\0'; i++)
 {
-_putchar(*(str + i));
-break i = \0
-i = i + 2;
+if (i % 2 == 0)
+{
+_putchar(str[i]);
+}
 }
 _putchar('\n');
 }
