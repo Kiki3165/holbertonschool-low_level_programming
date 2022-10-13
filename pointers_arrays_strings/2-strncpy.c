57506c15ec04f1d9c9ca8990d6abf2a8ee091dbf
kyllian <kikikou82@gmail.com>
2022-10-13 11:35:39 -0700

    task102

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - check the code
+ *
+ *@dest: cpy
+ *@src: cpy to
+ *@n: cpy too
+ *
+ * Return: Always 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+i = 0;
+while (i < n && *(src + i))
+{
+*(dest + i) = *(src + i);
+i++;
+}
+while (i < n)
+{
+*(dest + i) = '\0';
+i++;
+}
+return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,6 +5,8 @@
 
 int _isalpha(int c);
 
+char *_strncpy(char *dest, char *src, int n);
+
 char *_strncat(char *dest, char *src, int n);
 
 char *_strcat(char *dest, char *src);
