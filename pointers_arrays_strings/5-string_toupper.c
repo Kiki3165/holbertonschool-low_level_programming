0d9157ae60e904eadd735491be035a8e95a644de
kyllian <kikikou82@gmail.com>
2022-10-13 14:40:12 -0700

    task105

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - check the code
+ *
+ * Return: Always 0.
+ */
+
+char *string_toupper(char *)
+{
+int i = 0, t;
+n = n - 1;
+while (i < n)
+{
+t = *(a + i);
+*(a + i) = *(a + n);
+*(a + n) = t;
+i++;
+n--;
+}
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -7,6 +7,8 @@ int _isalpha(int c);
 
 void reverse_array(int *a, int n);
 
+char *string_toupper(char *);
+
 int _strcmp(char *s1, char *s2);
 
 char *_strncpy(char *dest, char *src, int n);
