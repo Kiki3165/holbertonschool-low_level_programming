b79c6d6e2cd4acd20acf4066721e8d1c504e0828
kyllian <kikikou82@gmail.com>
2022-10-13 11:54:04 -0700

    task103

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcmp - check the code
+ *
+ *@s1:compare string
+ *@s2: compare strings
+ *
+ * Return: Always 0.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int i = 0;
+while (*(s1 + i) && *(s2 + i) && (*(s1 + i) == *(s2 + i)))
+i++;
+return (*(s1 + i) - *(s2 + i));
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,6 +5,8 @@
 
 int _isalpha(int c);
 
+int _strcmp(char *s1, char *s2);
+
 char *_strncpy(char *dest, char *src, int n);
 
 char *_strncat(char *dest, char *src, int n);
