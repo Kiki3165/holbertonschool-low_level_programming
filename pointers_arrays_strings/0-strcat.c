46f609021edd7cc62a99053ad46ece123bac3ac6
kyllian <kikikou82@gmail.com>
2022-10-13 01:42:48 -0700

    task100

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -13,9 +13,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-char dest[];
-char src[];
-_strcat(src,dest);
+int i = 0;
+int j = 0;
+for (; dest[i] != '\o'; i++)
+{
+}
+for (j = 0; src[j] != '\0'; j++)
+{
+dest[i + 2] = src[j]
+}
 _putchar('\n');
-return (dest + src);
+return (dest);
 }
