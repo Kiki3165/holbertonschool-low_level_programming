39625648ffc2c5f58f96fad68f7ea3236b269058
kyllian <kikikou82@gmail.com>
2022-10-14 01:16:39 -0700

    task106

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -9,22 +9,22 @@
  * Return: Always 0.
  */
 
-char *cap_string(char *)
+char *cap_string(char *l)
 {
 int i = 0, j;
 char a[] = " \t\n,;.!?\"(){}";
-while (*(s + i))
+while (l(s + i))
 {
-if (*(s + i) >= 'a' && *(s + i) <= 'z')
+if (l(s + i) >= 'a' && l(s + i) <= 'z')
 {
 if (i == 0)
-*(s + i) -= 'a' - 'A';
+l(s + i) -= 'a' - 'A';
 else
 {
 for (j = 0; j <= 12; j++)
 {
-if (a[j] == *(s + i - 1))
-*(s + i) -= 'a' - 'A';
+if (a[j] == l(s + i - 1))
+l(s + i) -= 'a' - 'A';
 }
 }
 }
