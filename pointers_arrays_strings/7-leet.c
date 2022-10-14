d7a207631da729c407362a96122f6692e0bee3db
kyllian <kikikou82@gmail.com>
2022-10-14 08:01:09 -0700

    task107

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+
+char *leet(char *)
+{
+int i; x;
+char a[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L',};
+char b[] = {'4', '3', '0', '7', '1'};
+for (i = 0; n[i] != '\0'; i++)
+{
+for (x = 0; x < 10; x++)
+{
+if (n[i] == a[x])
+{
+n[i] = b[x / 2];
+break;
+}
+}
+}
+return (n);
+}
