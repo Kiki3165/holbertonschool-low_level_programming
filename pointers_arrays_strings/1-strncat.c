84668e88c3b8bc5ba948233e365815ce732e3971
kyllian <kikikou82@gmail.com>
2022-10-16 04:09:50 -0700

    task101

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -13,18 +13,17 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-int i = 0;
-int j = 0;
-int k;
-for (i = 0; dest[i] != '\0'; i++)
+int i, j;
+i = j = 0;
+while (*(dest + i))
+i++;
+while (j < n && *(src + j))
 {
-k = i + 1;
-
-for (j = 0; j < n; j++)
-{
-dest[k] = src[j];
-k++;
+*(dest + i) = *(src + j);
+i++;
+j++;
 }
-}
+if (j < n)
+*(dest + i) = *(src + j);
 return (dest);
 }
