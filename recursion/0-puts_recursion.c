f7ae82a1b203f3f7caf20ebb9d8aae91016b8d3f
kyllian <kikikou82@gmail.com>
2022-10-17 01:34:19 -0700

    task0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - check the code
+ *
+ *@s: print string
+ *
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+if (s <= 1)
+return (1);
+return (s * factorial(n - 1));
+_putchar(s);
+putchar('\n');
+}
