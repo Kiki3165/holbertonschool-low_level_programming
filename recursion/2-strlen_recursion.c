094e4292423712acc649d81834bf98e2bf3ca449
kyllian <kikikou82@gmail.com>
2022-10-17 09:48:39 -0700

    task2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -11,12 +11,7 @@
 
 int _strlen_recursion(char *s)
 {
-int i = 0;
-if (s[0] != '\0')
-{
-s++;
-_strlen_recursion(s);
-i++;
-}
+if (s == '\0')
 return (0);
+return (1 + _strlen_recursion(s + 1));
 }
