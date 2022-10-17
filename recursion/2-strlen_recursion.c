86390601dde7a1c59335d215b05d262f34653dc2
kyllian <kikikou82@gmail.com>
2022-10-17 09:37:15 -0700

    task2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - check the code
+ *
+ *@s: return length
+ *
+ * Return: Always 0.
+ */
+
+int _strlen_recursion(char *s)
+{
+int i = 0;
+if (s[0] != '\0')
+{
+s++;
+i++;
+}
+return (0);
+}
--- recursion/main.h
+++ recursion/main.h
@@ -5,6 +5,8 @@
 
 int _isalpha(int c);
 
+int _strlen_recursion(char *s);
+
 void _puts_recursion(char *s);
 
 void _print_rev_recursion(char *s);
