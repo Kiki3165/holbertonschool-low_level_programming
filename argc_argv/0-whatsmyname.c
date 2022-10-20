27aaa46ad657e4454e7e24f691639c57e9b9cecf
kyllian <kikikou82@gmail.com>
2022-10-20 01:14:40 -0700

    task0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ *@argc: print
+ *@argv: print to
+ * Return: Always 0.
+ */
+
+int main (int argc, char argv[])
+{
+int i;
+for (i = 0, i < argc ; i++)
+{
+printf("%s, argv[i]");
+}
+printf("\n");
+}
