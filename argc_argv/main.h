6f9421cfc437e47e5796db0424bc83b73ec00bc6
kyllian <kikikou82@gmail.com>
2022-10-20 01:16:59 -0700

    task0

---
+++ argc_argv/main.h
@@ -0,0 +1,102 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+int _isalpha(int c);
+
+int _sqrt_recursion(int n);
+
+int _sqrt_recursion(int n);
+
+int _pow_recursion(int x, int y);
+
+int factorial(int n);
+
+int _strlen_recursion(char *s);
+
+void _puts_recursion(char *s);
+
+void _print_rev_recursion(char *s);
+
+void print_chessboard(char (*a)[8]);
+
+char *_strstr(char *haystack, char *needle);
+
+char *_strpbrk(char *s, char *accept);
+
+unsigned int _strspn(char *s, char *accept);
+
+char *_strchr(char *s, char c);
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+char *_memset(char *s, char b, unsigned int n);
+
+char *leet(char *);
+
+char *cap_string(char *);
+
+void reverse_array(int *a, int n);
+
+char *string_toupper(char *);
+
+int _strcmp(char *s1, char *s2);
+
+char *_strncpy(char *dest, char *src, int n);
+
+char *_strncat(char *dest, char *src, int n);
+
+char *_strcat(char *dest, char *src);
+
+char *_strcpy(char *dest, char *src);
+
+void puts_half(char *str);
+
+void print_array(int *a, int n);
+
+void puts2(char *str);
+
+void rev_string(char *s);
+
+void print_rev(char *s);
+
+void _puts(char *str);
+
+int _strlen(char *s);
+
+void swap_int(int *a, int *b);
+
+void reset_to_98(int *n);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+int mul(int a, int b);
+
+void print_numbers(void);
+
+int _isdigit(int c);
+
+int _isupper(int c);
+
+void times_table(void);
+
+void jack_bauer(void);
+
+int print_last_digit(int);
+
+int _abs(int);
+
+int print_sign(int n);
+
+int _putchar(char c);
+
+void print_alphabet(void);
+
+int _islower(int c);
+
+void print_alphabet_x10(void);
+
+#endif
