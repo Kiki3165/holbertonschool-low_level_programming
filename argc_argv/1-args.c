1b7072e344af6c68c7c65eb259c498282430988d
kyllian <kikikou82@gmail.com>
2022-10-20 03:19:06 -0700

    task1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - check the code
+ *
+ *@argc: print string
+ *@argv:  print too
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv)
+{
+int i;
+printf("%d\n", argc);
+for (i = 0; i < argc; i++)
+{
+printf("%d\n", argv[i]);
+}
+return (0);
+}
