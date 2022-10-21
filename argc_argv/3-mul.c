309fe9ce98d8b0da680145eca06c33abb802b64f
kyllian <kikikou82@gmail.com>
2022-10-21 11:24:20 -0700

    task3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function
+ * @argc: multiply
+ * @argv: multiply to
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+int i, result = 1;
+if (argc < 3)
+printf("Error\n");
+else
+{
+for (i = 1; i < argc; i++)
+result *= atoi(argv[i]);
+printf("%d\n", result);
+}
+return (0);
+}
