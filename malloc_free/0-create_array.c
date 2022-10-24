0306ed4adf7f55c9b29fa2950bf3356e8f0b1a76
kyllian <kikikou82@gmail.com>
2022-10-24 06:28:36 -0700

    task0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -16,16 +16,12 @@ char *create_array(unsigned int size, char c)
 {
 char *p;
 unsigned int i = 0;
- 
 if (size == 0)
 return (NULL);
- 
 p = malloc(sizeof(char) * size);
 if (p == NULL)
 return (NULL);
- 
 for (; i < size; i++)
 p[i] = c;
-
 return (p);
 }
