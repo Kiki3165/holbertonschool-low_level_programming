af125bef180bbd0ce96ed674b5608b61bf6c08b6
kyllian <kikikou82@gmail.com>
2022-10-27 01:45:10 -0700

    task0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - check code
+ *
+ *@b: allocate memory
+ *
+ *
+ *
+ * Return: 0
+ */
+
+void *malloc_checked(unsigned int b)
+{
+int *p;
+p = (int*) malloc(b * sizeof(int));
+return (*p);
+}
