7d02f39fedf7796d4f7f44e16998888fbd15ca04
kyllian <kikikou82@gmail.com>
2022-11-01 03:52:31 -0700

    task1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -4,18 +4,18 @@
 
 /**
  * init_dog - Entry point
- * Description: 'initialize a variable of type struct dog'
- * Return: nothing
- * @d: dog struct parameter
- * @name: char parameter
- * @age: float parameter
- * @owner: char parameter
+ * @d: d
+ * @name: name
+ * @age: age
+ * @owner: owner
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 if (d != NULL)
-(*d).name = name
-d->age = age
-d->owner = owner
+{
+(*d).name = name;
+d->age = age;
+d->owner = owner;
+}
 }
