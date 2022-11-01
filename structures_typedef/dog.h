e709c2fc772023b30e24844f99801a540084a861
kyllian <kikikou82@gmail.com>
2022-11-01 04:26:54 -0700

    task3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef DOG_C_H
+#define DOG_C_H
 
 /**
 * struct dog - check the code
@@ -10,7 +10,7 @@
 * Return: Always 0.
 */
 
-struct dog
+struct dog_t
 {
 char *name;
 float age;
