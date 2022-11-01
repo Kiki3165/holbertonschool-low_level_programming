500319db773b1a1aa55cb3064603c7d5261a08ca
kyllian <kikikou82@gmail.com>
2022-11-01 03:24:09 -0700

    task0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,12 +1,10 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-
+* main - check the code
+*
+* Return: Always 0.
+*/
 struct dog
 {
 char *name;
