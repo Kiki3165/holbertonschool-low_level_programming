797c3b3ce467d0f21dc3fbf4f6ef837acfbc9d9e
kyllian <kikikou82@gmail.com>
2022-11-01 03:56:22 -0700

    task1

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -15,7 +15,7 @@ struct dog
 char *name;
 float age;
 char *owner;
-} dog_t;
+};
 void init_dog(struct dog_t *d, char *name, float age, char *owner);
 void print_dog(struct dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
