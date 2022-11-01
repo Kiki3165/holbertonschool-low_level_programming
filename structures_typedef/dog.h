2778c2fc8e1a2515f4476d1525fa053a9a92e332
kyllian <kikikou82@gmail.com>
2022-11-01 03:42:26 -0700

    task0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,13 +1,20 @@
-#include "main.h"
+#ifndef DOG_H
+#define DOG_H
 
 /**
 * struct dog - check the code
+*@name: name
+*@age: age
+*@owner: owner
 *
 * Return: Always 0.
 */
+
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+
+#endif
