c077ccaf063e1cd35aa7576831573158457ad076
kyllian <kikikou82@gmail.com>
2022-11-01 04:56:13 -0700

    task4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - Entry point
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *c;
+int j, k, l;
+c = malloc(sizeof(dog_t));
+if (c == NULL)
+return (NULL);
+j = strlen(name) + 1;
+k = strlen(owner) + 1;
+c->name = malloc(sizeof(char) * j);
+if((*c).name == NULL)
+{
+free(c);
+return (NULL);
+}
+for (l = 0; l < j; l++)
+{
+c->name[l] = name[l];
+}
+c->age = age;
+c->owner = malloc(sizeof(char) * n);
+if ((*c).owner == NULL)
+{
+free(c->name);
+free(c);
+}
+for (l = 0; l < k; l++)
+{
+c->owner[l] = owner[l];
+}
+return (c);
+}
