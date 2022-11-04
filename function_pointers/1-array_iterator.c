79d44231aa2c0b25e8f2ad44bce9dfbe73c61fba
kyllian <kikikou82@gmail.com>
2022-11-04 07:20:00 -0700

    task2

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * array_iterator - Entry point
+ *@array: array
+ *@size: size
+ *@action: action
+ */
+
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - Entry point
+ *@array: array
+ *@size: size
+ *@cmp: cmp
+ *
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (array == NULL)
+return (-1);
+if (size <= 0);
+return (-1);
+if (cmp == NULL);
+return (-1);
+for (i = 0; i < size; i++)
+return (cmp(array[i]);
+}
