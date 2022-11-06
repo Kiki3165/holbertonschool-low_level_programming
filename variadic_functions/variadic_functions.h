e89a99e87cc1a000f8f45260b7e698d134369fe3
Kiki3165 <113670617+Kiki3165@users.noreply.github.com>
2022-11-06 22:51:48 +0100

    Update variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,10 +2,10 @@
 #define VARIADIC_FUNCTIONS_H
 
 void print_numbers(const char *separator, const unsigned int n, ...);
-<<<<<<< HEAD
+
 void print_strings(const char *separator, const unsigned int n, ...);
-=======
+
 void print_strings(const char *separator, const unsigned int n, ...)
->>>>>>> ff11b69c168e1873206275113e9e62c4cd1f6885
+
 int sum_them_all(const unsigned int n, ...);
 #endif
