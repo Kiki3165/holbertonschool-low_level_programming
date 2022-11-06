b6e9b34a5ad801ae725f154df47797365d7be7b9
kyllian <kikikou82@gmail.com>
2022-11-06 06:23:38 -0800

    task0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -13,11 +13,12 @@ int sum_them_all(const unsigned int n, ...)
 int o = 0;
 unsigned int i = 0;
 va_list ap;
-if (n == 0)
-return (0);
 va_start(ap, n);
 while (s < n)
+{
 o += (int) va_arg(ap, int);
+i++;
+}
 va_end(ap);
 return (o);
 }
