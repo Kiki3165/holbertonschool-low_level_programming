6c2cd070135a7a63982cf3532d2514b54439c53c
kyllian <kikikou82@gmail.com>
2022-11-06 10:20:46 -0800

    task2

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - check the code
+ *
+ *@separator: separator
+ *@n: n
+ *
+ * Return: Always 0.
+ */
