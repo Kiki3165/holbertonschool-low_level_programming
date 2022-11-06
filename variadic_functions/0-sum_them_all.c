5d8aced95783579fb93ac00848fbe0707873f56b
kyllian <kikikou82@gmail.com>
2022-11-06 05:13:31 -0800

    task0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - check the code
+ *@n: n
+ * Return: Always 0.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+if (n == 0 )
+{
+return (0);
+}
+return (n);
+}
