b8a202fc20d0605fd264871b2df4795f249d3000
kyllian <kikikou82@gmail.com>
2022-11-07 09:13:29 -0800

    task6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
+ * print_line - check the code
+ *@n: n
  * Return: Always 0.
  */
 
 void print_line(int n)
 {
-_puts(str);
-return();
+    int i
+    for (i = 0; i <= n; i++)
+    {
+        _putchar("\n");
+    }
+    if (n == 0)
+    _putchar("\n");
 }
