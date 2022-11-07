03273ce2c6f27741f3e0d57aac2a984dac693d48
kyllian <kikikou82@gmail.com>
2022-11-07 03:14:20 -0800

    task3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - check the code
+ *
+ * @format: format
+ *
+ * Return: Always 0.
+ */
+
+void print_all(const char * const format, ...)
+{
+va_list ap;
+char *s;
+float f;
+int i;
+while (i == s)
+i++;
+if (s == NULL)
+{
+s = "(nil)";
+printf("%s\n", s);
+}
+for (i = 0; i < format; i++)
+{
+s = va_arg(ap, char*)
+if ( s == (format - 1))
+}
+}
