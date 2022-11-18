6ed1675fe93ee5c309512aeded6df6bfba299885
kyllian <kikikou82@gmail.com>
2022-11-18 07:47:30 -0800

    fix_betty

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -11,13 +11,12 @@
 
 size_t print_listint(const listint_t *h)
 {
-	int i = 0;
-
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		i++;
-	}
-	return (i);
+int i = 0;
+while (h)
+{
+printf("%d\n", h->n);
+h = h->next;
+i++;
+}
+return (i);
 }
