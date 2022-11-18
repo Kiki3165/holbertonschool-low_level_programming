830e1c8d3da5e5b8d2155cbb3e28877031967cfb
kyllian <kikikou82@gmail.com>
2022-11-18 05:57:14 -0800

    task1

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -9,7 +9,7 @@
 
 void print_binary(unsigned long int n)
 {
-    if (n > 1)
-    print_binary(n / 2)
-    printf("%d", n % 2);
+    unsigned i;
+    for (i = 1 << 31; i > 0; i >>= 1)
+        printf("%u", !!(n & i));
 }
