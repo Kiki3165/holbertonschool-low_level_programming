55b134cddb31a8bbdd32f12d1a11314b10d1f0c4
kyllian <kikikou82@gmail.com>
2022-11-19 10:13:21 -0800

    bitman

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
+int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
