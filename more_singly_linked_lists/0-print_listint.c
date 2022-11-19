00a29b874d6975faaf69c6d746ee4b7ef0208a26
kyllian <kikikou82@gmail.com>
2022-11-19 09:52:46 -0800

    task0

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -11,12 +11,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-int i = 0;
-while (h)
-{
-printf("%d\n", h->n);
-h = h->next;
-i++;
-}
-return (i);
+	int nb = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nb++;
+	}
+	return (nb);
 }
