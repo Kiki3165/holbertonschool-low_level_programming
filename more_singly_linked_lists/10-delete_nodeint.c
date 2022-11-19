b88e18f3079fc738daa409a909b5b61eb5da568b
kyllian <kikikou82@gmail.com>
2022-11-19 14:25:39 -0800

    task10

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -10,3 +10,31 @@
  */
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *i = *head;
+	listint_t *p = *head;
+
+	if (*head == NULL)
+		return (-1);
+	else if (index == 0)
+	{
+		*head = p->next;
+		free(p);
+	}
+	else
+	{
+		i = p;
+		p = p->next;
+		while (index != 1)
+		{
+			if (p == NULL || p->next == NULL)
+				return (-1);
+			i = p;
+			p = p->next;
+			index--;
+		}
+		i->next = p->next;
+		free(p);
+	}
+	return (1);
+}
