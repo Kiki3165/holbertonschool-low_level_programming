fc6cc0c00be31d0d83e13d6c9a10b6d8dbdbc367
kyllian <kikikou82@gmail.com>
2022-11-19 09:31:01 -0800

    task9

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = idx;
+	listint_t *node;
+	listint_t *ptr = *head;
+
+	if (ptr == NULL)
+	{
+		node = malloc(sizeof(listint_t));
+		if (node == NULL)
+			return (NULL);
+		node->n = n;
+		node->next = *head;
+		*head = node;
+		return (node);
+	}
+	if (i == 0)
+	{
+		node = malloc(sizeof(listint_t));
+		if (node == NULL)
+			return (NULL);
+		node->n = n;
+		node->next = *head;
+		*head = node;
+		return (node);
+	}
+	while (i != 1)
+	{
+		if (ptr->next == NULL)
+			return (NULL);
+		ptr = ptr->next;
+		i--;
+	}
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+	node->next = ptr->next;
+	ptr->next = node;
+	return (node);
+}
