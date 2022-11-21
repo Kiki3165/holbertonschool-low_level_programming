004394e3519e32786e834b02f0ca6c59e14f95b5
kyllian <kikikou82@gmail.com>
2022-11-21 03:29:08 -0800

    task0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
 
 /**
  * main - check the code
@@ -10,12 +14,23 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    char buffer[letters];
-    fd = open("filename", "r");
-    if (fd == NULL);
-    return (o);
-    read(fd, buffer, letters);
-    close(fd);
-    return (filename);
+        FILE* ptr;
+    char ch;
+    ptr = fopen("test.txt", "r");
+
+    if (NULL == ptr) {
+        printf("file can't be opened \n");
+    }
+ 
+    printf("content of this file are \n");
+
+
+    do {
+        ch = fgetc(ptr);
+        printf("%c", ch);
+
+
+    } while (ch != EOF);
+    fclose(ptr);
+    return 0;
 }
