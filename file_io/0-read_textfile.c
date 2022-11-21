ae04e82031a1f419be78bb9a35de0c3a392391ce
kyllian <kikikou82@gmail.com>
2022-11-21 06:46:40 -0800

    task0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -26,7 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
     fd = open(filename, O_RDONLY);
     size = read(fd, buf, letters);
     buf[size] = 0;
-    write(STDOUT_FILENO, &buf, size, STDOUT_FILENO);
+    write(STDOUT_FILENO, &buf, size,);
     close(fd);
     return (0);
 }
