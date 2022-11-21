ff6d7198c861734d4c2d043b6467149847c43a97
kyllian <kikikou82@gmail.com>
2022-11-21 11:09:48 -0800

    task1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,28 +16,32 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    	int file, i = 0;
+    	int wr_perm, i, fd, new_file;
 
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-	if (file == -1)
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+	if (fd == -1)
 		return (-1);
-
-	while (text_content[i])
-		i++;
 
 	if (text_content == NULL)
 	{
-		close(file);
-		return (-1);
-	}
-	else
-	{
-		write(STDOUT_FILENO, file, i);
+		new_file = open("test.new_file", O_CREAT | O_RDONLY);
+		if (new_file == -1)
+			return (-1);
+		text_content = malloc(sizeof(char) * 1);
+		read(new_file, text_content, 0);
+
 	}
 
-	close(file);
+	for (i = 0; text_content[i]; i++)
+		;
+
+	wr_perm = write(fd, text_content, i);
+	if (wr_perm == -1)
+		return (-1);
+	close(new_file);
+	close(fd);
 	return (1);
 }
