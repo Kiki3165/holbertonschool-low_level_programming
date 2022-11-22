3307b88bac6b703a4aae911d83492c33fb99007e
kyllian <kikikou82@gmail.com>
2022-11-22 10:13:29 -0800

    task0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - check the code
+ *@h:h
+ * Return: Always EXIT_SUCCESS.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+int i = 0;
+const dlistint_t *p = h;
+while (p != NULL)
+{
+if (p->str == NULL)
+{
+printf("[o] (nil)\n");
+i++;
+p = p->next;
+}
+printf("[%d] %s\n", p->len, p->str);
+i++;
+p = p->next;
+}
+return (i);
+}
