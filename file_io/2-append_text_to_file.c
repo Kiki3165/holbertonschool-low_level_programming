c1e058b3d8a44aa31e32008ed4bc691e5cf970af
kyllian <kikikou82@gmail.com>
2022-11-22 05:45:21 -0800

    segfault

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -27,13 +27,13 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		return (1);
+		text_content = "";
 	}
 	else
 	{
 		wr = write(file, text_content, i);
 	}
-	
+
 	if (wr == -1)
 		return (-1);
 
