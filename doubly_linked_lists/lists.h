2373d55d9cb65f815d18b4cd854773435c7b4c20
kyllian <kikikou82@gmail.com>
2022-11-22 10:18:22 -0800

    task0

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,6 +1,13 @@
 #ifndef LIST_H
 #define LIST_H
 
+typedef struct list_s
+{
+  char *str;
+  unsigned int len;
+  struct list_s *next;
+} dlistint_t;
+
 size_t print_dlistint(const dlistint_t *h);
 
 #endif
