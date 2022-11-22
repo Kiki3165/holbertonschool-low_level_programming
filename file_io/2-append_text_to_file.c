7797836ba7cf4266374535cb1c522ca633af4913
kyllian <kikikou82@gmail.com>
2022-11-22 05:41:59 -0800

    segfault

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -8,7 +8,7 @@
 #include <unistd.h>
 
 /**
- * append_to_file - check the code
+ * append_text_to_file - check the code
  *@filename:filename
  *@text_content:text_content
  * Return: Always 0.
@@ -25,11 +25,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (file == -1)
 		return (-1);
 
-
-
-	while (text_content[i])
-		i++;
-
 	if (text_content == NULL)
 	{
 		return (1);
@@ -38,7 +33,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		wr = write(file, text_content, i);
 	}
-
+	
 	if (wr == -1)
 		return (-1);
 
