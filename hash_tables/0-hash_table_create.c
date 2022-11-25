0c77a8ee5cca8f874415a1e01f02313c9ba70d28
kyllian <kikikou82@gmail.com>
2022-11-25 07:10:45 -0800

    task0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -4,20 +4,20 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code for
- *
+ * hash_table_create - check the code for
+ *@size:size
  * Return: Always EXIT_SUCCESS.
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *t;
-    t = malloc (sizeof(hash_table_t));
-    if (t == NULL)
-    {
-    return (NULL);
-    }
-    t->size = size;
-    t->array = malloc(size * sizeof(hash_node_t *));
-    return (t);
+hash_table_t *t;
+t = malloc (sizeof(hash_table_t));
+if (t == NULL)
+{
+return (NULL);
+}
+t->size = size;
+t->array = malloc(size * sizeof(hash_node_t *));
+return (t);
 }
