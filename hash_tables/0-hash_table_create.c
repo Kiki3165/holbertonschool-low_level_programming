cfb03666e7a20ce49610fb9cd5c07a4d0b5accf0
kyllian <kikikou82@gmail.com>
2022-11-24 16:21:54 -0800

    firstcommit

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * main - check the code for
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *t;
+    t = malloc (sizeof(hash_table_t);
+    if (t == NULL)
+    {
+    return (NULL);
+    }
+    t->size = size;
+    t->array = malloc(size * sizeof(hash_node_t *));
+    return (t);
+}
